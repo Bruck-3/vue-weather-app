d83fa4bdba0cf2d5291ada081c170523d8a6c63a
bk-26 <49018192+bk-26@users.noreply.github.com>
2020-11-29 16:19:52 +0300

    commit

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <Index />
+  </v-app>
+</template>
+
+<script>
+import Index from '../pages/index.vue'
+export default {
+  components: {
+    Index,
+  },
+  data() {
+    return {}
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <v-card width="400px" height="600px">
+      <v-img src="https://via.placeholder.com/300x300"></v-img>
+    </v-card>
+    <p></p>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
