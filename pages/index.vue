96aca5a6263a0f7906262a42ae0d5225cb7b27e0
bk-26 <49018192+bk-26@users.noreply.github.com>
2020-11-29 20:45:18 +0300

    Functionality

--- pages/index.vue
+++ pages/index.vue
@@ -12,10 +12,20 @@
             ></v-text-field>
           </v-form>
 
-          <v-img src="https://via.placeholder.com/300x300"></v-img>
-          <p class="text-center text-h6">{{ cityName }}</p>
-          <p class="text-center text-subtitle-1">{{ weatherCondition }}</p>
-          <p class="text-center text-h4">{{ temperature }}&deg;C</p>
+          <div v-if="showDetails">
+            <v-img :src="image"></v-img>
+            <div class="text-center">
+              <v-icon class="icon-thing">mdi-face</v-icon>
+            </div>
+
+            <p class="text-center text-h6">{{ weatherDetails.cityName }}</p>
+            <p class="text-center text-subtitle-1">
+              {{ weatherDetails.weatherCondition }}
+            </p>
+            <p class="text-center text-h4">
+              {{ weatherDetails.temperature }}&deg;C
+            </p>
+          </div>
         </v-card>
       </v-col>
     </v-row>
@@ -27,16 +37,65 @@ export default {
   data() {
     return {
       input: null,
-      cityName: 'City Name',
-      weatherCondition: 'Weather Condtion',
-      temperature: 'TEMP',
-      apikey: 'FXC7yeBIR5XvzlEUMg09xxgzcbs2we4J',
+      showDetails: false,
+      image: 'https://via.placeholder.com/300x200',
+      weatherDetails: {
+        cityName: '',
+        weatherCondition: '',
+        temperature: '',
+      },
+      apiKey: 'FXC7yeBIR5XvzlEUMg09xxgzcbs2we4J',
     }
   },
   methods: {
     clickLocation() {
       let location = this.input.trim()
+      this.getCityKey(location)
+    },
+    getCityKey(location) {
+      const base =
+        'http://dataservice.accuweather.com/locations/v1/cities/search?'
+
+      this.$axios
+        .get(base + `apikey=${this.apiKey}&q=${location}`)
+        .then((response) => {
+          if (response.data[0]) {
+            this.weatherDetails.cityName =
+              response.data[0].AdministrativeArea.EnglishName
+            this.getCityWeather(response.data[0].Key)
+            this.input = null
+          } else {
+            console.log("Couldn't find The city You are looking for")
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    getCityWeather(key) {
+      const base = `http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/${key}?`
+      this.$axios
+        .get(base + `apikey=${this.apiKey}`)
+        .then((response) => {
+          if (response.data[0].IsDayLight) {
+          } else {
+          }
+          this.weatherDetails.weatherCondition = response.data[0].IconPhrase
+          this.weatherDetails.temperature =
+            response.data[0].Temperature.UnitType
+          this.showDetails = true
+        })
+        .catch((error) => {
+          console.log(error)
+        })
     },
   },
 }
 </script>
+<style lang="css">
+.icon-thing {
+  position: relative;
+  width: 100px;
+  top: -30px;
+}
+</style>
