ddf8b105d0205aa8452adcbe4518efc6182385b1
bk-26 <49018192+bk-26@users.noreply.github.com>
2020-11-29 19:38:06 +0300

    message

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,42 @@
 <template>
   <v-container>
-    <v-card width="400px" height="600px">
-      <v-img src="https://via.placeholder.com/300x300"></v-img>
-    </v-card>
-    <p></p>
+    <v-row>
+      <v-col>
+        <v-card width="400px">
+          <v-card-text class="text-center text-h4"> Weather App</v-card-text>
+          <v-form @submit.prevent="clickLocation">
+            <v-text-field
+              outlined
+              v-model="input"
+              label="Enter Location"
+            ></v-text-field>
+          </v-form>
+
+          <v-img src="https://via.placeholder.com/300x300"></v-img>
+          <p class="text-center text-h6">{{ cityName }}</p>
+          <p class="text-center text-subtitle-1">{{ weatherCondition }}</p>
+          <p class="text-center text-h4">{{ temperature }}&deg;C</p>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      input: null,
+      cityName: 'City Name',
+      weatherCondition: 'Weather Condtion',
+      temperature: 'TEMP',
+      apikey: 'FXC7yeBIR5XvzlEUMg09xxgzcbs2we4J',
+    }
+  },
+  methods: {
+    clickLocation() {
+      let location = this.input.trim()
+    },
+  },
+}
 </script>
